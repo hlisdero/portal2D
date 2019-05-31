090c971f17e49a8b2ad0fae0a2afd82966287a06
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2019-05-31 18:09:35 -0300

    Use world manifold to get localnormal

--- src/server/entities/end_barrier.cpp
+++ src/server/entities/end_barrier.cpp
@@ -7,14 +7,17 @@ void EndBarrierEntity::handleContactWith(Entity * entity, b2Contact * contact, b
 	if(entity->getType() == TYPE_PLAYER) {
 		PlayerEntity * player = entity->as<PlayerEntity>();
 
+		b2WorldManifold manifold;
+		contact->GetWorldManifold(&manifold);
+
 		if(inContact) {
-			this->contacts[player] = contact->GetManifold()->localNormal;
+			this->contacts[player] = manifold.normal;
 		} else {
 			auto iterator = this->contacts.find(player);
 
 			if(iterator != this->contacts.end()) {
 				if(iterator->second == 
-					-1 * contact->GetManifold()->localNormal) {
+					-1 * manifold.normal) {
 					// Player went through
 					endZone.playerWentTroughBarrier(player);
 				} else {
