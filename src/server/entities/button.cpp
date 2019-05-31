b6d2323c7c2bf8571ea22d48b64a71e9152ff9ca
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2019-05-31 02:51:33 -0300

    Use Subscribable on button and energy receiver

--- src/server/entities/button.cpp
+++ src/server/entities/button.cpp
@@ -1,24 +1,14 @@
 #include "server/entities/button.h"
 
 ButtonEntity::ButtonEntity(const float x, const float y) :
-	Entity(TYPE_BUTTON, x, y, 0), WithState(STATE_DISABLED) {}
+	Entity(TYPE_BUTTON, x, y, 0), WithSubscribableState(STATE_DISABLED) {}
 
 ButtonEntity::ButtonEntity(YAML::Node yaml) :
-	Entity(TYPE_BUTTON, yaml), WithState(STATE_DISABLED) {}
+	Entity(TYPE_BUTTON, yaml), WithSubscribableState(STATE_DISABLED) {}
 
 void ButtonEntity::handleContactWith(Entity * other, b2Contact *, bool inContact) {
 	if(other->getType() == TYPE_PLAYER || other->getType() == TYPE_ROCK) {
 		// TODO enhance the detection
 		this->setState(inContact ? STATE_ENABLED : STATE_DISABLED);
-	}
-}
-
-void ButtonEntity::registerDoor(DoorEntity * door) {
-	this->doors.push_back(door);
-}
-
-void ButtonEntity::broadcastChange() {
-	for(uint i = 0; i < this->doors.size(); i++) {
-		this->doors[i]->updateState();
 	}
 }
