40a8e356b1b390206dd186c8dc629db3d1a4c7b2
Horacio Lisdero <lisderoh@gmail.com>
2019-06-03 01:53:19 -0300

    Modified Map class and map files to allow strings as types

--- include/server/map.h
+++ include/server/map.h
@@ -3,12 +3,20 @@
 
 #include "Box2D/Box2D.h"
 
+#include <map>
+#include <string>
 #include <vector>
 
 #include "yaml-cpp/yaml.h"
-#include "common/entities/entity.h"
 #include "server/end_zone.h"
 #include "server/door_logica.h"
+#include "common/entities/entity.h"
+#include "server/entities/door.h"
+#include "server/entities/energy_emittor.h"
+#include "server/entities/energy_receiver.h"
+#include "server/entities/button.h"
+#include "server/entities/rock.h"
+#include "server/entities/end_barrier.h"
 
 class Map {
 public:
@@ -35,6 +43,8 @@ private:
 	void loadSettings(YAML::Node yaml);
 	void loadEntities(YAML::Node yaml);
 	Entity * createEntity(YAML::Node yaml);
+
+    EntityType getEntityType(std::string str);
 };
 
 #endif  // MAP_H
