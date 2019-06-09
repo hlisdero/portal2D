7c9577bde7898e1565454b5355bd46af99d3c44e
Horacio Lisdero <lisderoh@gmail.com>
2019-06-09 19:08:50 -0300

    Disable login screen for convenience

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include "client/client.h"
 #include "client/login/login.h"
 
-int main(int argc, char *argv[]) {
+int main(/*int argc, char *argv[]*/) {
     try {
         Login login;
-        login.run(argc, argv);  // Comentar para usar defaults
-
+        // login.run(argc, argv);
         // Tamaño del mundo en metros 53.32 x 40
         Client client(login.getHostname(), login.getPort(), 53.32, 40);
         client.run();
         return 0;
-    } catch(std::runtime_error err) {
+    } catch (std::runtime_error err) {
         std::cerr << err.what() << std::endl;
         return 1;
     }
