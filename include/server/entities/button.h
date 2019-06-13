a1ca7fbcb2a1d24a2db4402351177036d5b5d57a
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2019-06-13 14:39:17 -0300

    Support multi-contact on the buttons

--- include/server/entities/button.h
+++ include/server/entities/button.h
@@ -1,7 +1,7 @@
 #ifndef BUTTON_ENTITY_H
 #define BUTTON_ENTITY_H
 
-#include <vector>
+#include <unordered_set>
 #include "Box2D/Box2D.h"
 
 #include "server/entities/entity.h"
@@ -13,6 +13,9 @@ public:
 	ButtonEntity(float x, float y, float rotation, GameEventCreator& gameEventCreator);
 
 	virtual void handleContactWith(Entity * other, b2Contact * contact, bool inContact) override;
+
+private:
+	std::unordered_set<b2Contact*> contacts;
 };
 
 #endif  // BUTTON_ENTITY_H
--- src/server/entities/button.cpp
+++ src/server/entities/button.cpp
@@ -3,9 +3,16 @@
 ButtonEntity::ButtonEntity(float x, float y, float rotation, GameEventCreator & gameEventCreator) :
 	Entity(TYPE_BUTTON, x, y, rotation), WithSubscribableState(STATE_DISABLED, gameEventCreator) {}
 
-void ButtonEntity::handleContactWith(Entity * other, b2Contact *, bool inContact) {
-	if (other->getType() == TYPE_PLAYER || other->getType() == TYPE_ROCK) {
-		// TODO enhance the detection
-		setState(inContact ? STATE_ENABLED : STATE_DISABLED);
+void ButtonEntity::handleContactWith(Entity * other, b2Contact * contact, bool inContact) {
+	if (other->getType() != TYPE_PLAYER && other->getType() != TYPE_ROCK) {
+		return;
 	}
+
+	if(inContact) {
+		contacts.insert(contact);
+	} else {
+		contacts.erase(contact);
+	}
+
+	setState(contacts.size() > 0 ? STATE_ENABLED : STATE_DISABLED);
 }
