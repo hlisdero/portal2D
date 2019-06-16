84eacf6529591e79c92dec26c4a412053beea7d3
Horacio Lisdero <lisderoh@gmail.com>
2019-06-15 22:44:36 -0300

    Minor whitespace change

--- src/client/objects/portal.cpp
+++ src/client/objects/portal.cpp
@@ -5,7 +5,7 @@ Portal::Portal(const Size& size, const Position& initial,
                PortalColor color) :
     Block(size, initial, settings, texture) {
 
-    if(color == COLOR_ORANGE) {
+    if (color == COLOR_ORANGE) {
     	sprite.addClip(1680, 27, 164, 164);
 	} else {
     	sprite.addClip(1862, 26, 164, 164);
